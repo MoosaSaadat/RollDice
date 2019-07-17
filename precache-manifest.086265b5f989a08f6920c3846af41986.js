7e9b84ff40dc8f34f85a9ac4d40c3094effd3079
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-17 12:12:33 +0500

    Updates

---
+++ precache-manifest.086265b5f989a08f6920c3846af41986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3692250591d845681d53457da0eb76ed",
+    "url": "/RollDice/index.html"
+  },
+  {
+    "revision": "90b4bdbda8dd7b3a35e1",
+    "url": "/RollDice/static/css/main.2b8eac1e.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/RollDice/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "90b4bdbda8dd7b3a35e1",
+    "url": "/RollDice/static/js/main.63c3bba0.chunk.js"
+  },
+  {
+    "revision": "4d3925c43e87c01a0005",
+    "url": "/RollDice/static/js/runtime~main.e16cc26d.js"
+  }
+]);
